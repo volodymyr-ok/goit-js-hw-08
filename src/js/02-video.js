d196e3d2e1b9213d59e40f4a23423ffbc20d7233
Volodymyr Ok <v.shark.t@gmail.com>
2022-10-14 00:55:28 +0300

    done hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+// function onPlay(data) {
+//   localStorage.setItem('videoplayer-current-time', data.seconds);
+// }
+
+function currentTimeInStorage(data) {
+  console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+player.on('timeupdate', throttle(currentTimeInStorage, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+const getEl = x => document.querySelector(x);
 
+const form = getEl('form');
+const email = getEl('[name="email"]');
+const message = getEl('[name="message"]');
+// const message = getEl('textarea');
+const storrage = { email: '', message: '' };
+
+form.addEventListener('input', throttle(makeStorage, 500));
+// form.addEventListener('input', makeStorage);
+populateInput();
+form.addEventListener('submit', resetStorage);
+
+function makeStorage(evt) {
+  storrage[evt.target.name] = evt.target.value;
+  const stringify = JSON.stringify(storrage);
+  localStorage.setItem('feedback-form-state', stringify);
+}
+
+function populateInput() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+
+  if (savedMessage) {
+    const parsed = JSON.parse(savedMessage);
+
+    email.value = parsed.email;
+    storrage.email = parsed.email;
+
+    message.value = parsed.message;
+    storrage.message = parsed.message;
+  }
+}
+
+function resetStorage(evt) {
+  evt.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+  console.log(storrage);
+
+  evt.currentTarget.reset();
+}
